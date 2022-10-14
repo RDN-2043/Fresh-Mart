7f48ac634782c41935659156a4a74eaced1de11b
Rafi Dinata <rafi.dinata2043@students.unila.ac.id>
2022-10-14 21:50:45 +0700

    DB

--- Database/weblanjut_freshmart.sql
+++ Database/weblanjut_freshmart.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.1.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 12, 2022 at 04:45 PM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.10
+-- Generation Time: Oct 14, 2022 at 04:50 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -40,6 +40,27 @@ CREATE TABLE `migrations` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `table_account`
+--
+
+CREATE TABLE `table_account` (
+  `id` int(5) NOT NULL,
+  `username` varchar(256) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `type` enum('seller','customer','','') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `table_account`
+--
+
+INSERT INTO `table_account` (`id`, `username`, `password`, `type`) VALUES
+(1, 'seller1', 'seller1', 'seller'),
+(2, 'customer1', 'customer1', 'customer');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `table_product`
 --
 
@@ -51,24 +72,16 @@ CREATE TABLE `table_product` (
   `available` int(3) NOT NULL,
   `star` int(1) NOT NULL,
   `price` int(12) NOT NULL,
-  `imgLink` varchar(256) NOT NULL
+  `imgLink` varchar(256) NOT NULL,
+  `id_user` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `table_product`
 --
 
-INSERT INTO `table_product` (`id`, `title`, `seller`, `description`, `available`, `star`, `price`, `imgLink`) VALUES
-(2, 'Timun Emas', 'Samsudin', 'Timun Emas yang biasa kita kenal dengan cerita dongeng itu, tidak hanya itu karena di Fresh Mart juga kami menjual Timun Emas yang merupakan asli buah. Ayo mari beli !', 5, 3, 75000, 'https://cdn.discordapp.com/attachments/776404266767745034/1029191795176915034/logo2.png'),
-(3, 'Timun', 'Pak Ghofar', 'Timun merupakan sayur segar bisa dimasak bisa juga di jus \"Ayo beli produk kami karena yang lain belum tentu berkualitas.\"', 3, 2, 50000, 'https://cdn.discordapp.com/attachments/776404266767745034/1029191794837172255/timun.png'),
-(4, 'Kangkung', 'Bu Haji', '\"Bingung cari sayur segar dimana? Hubungi Fresh Mart dan dapatkan berbagai menu terbaru dari kami.\"', 2, 5, 50000, 'https://cdn.discordapp.com/attachments/776404266767745034/1029191794459684914/kangkung.png'),
-(5, 'Rendang', 'Pak Abdul', '\"Halo agan dan sista silahkan datang ke warung. Kami menjual berbagai olahan lauk pauk.\"', 10, 4, 30000, 'https://cdn.discordapp.com/attachments/776404266767745034/1029191793104920586/rendang.jpg\r\n'),
-(6, 'Opor Ayam', 'Haji  Fachru', '\"Bingung mau cari lauk hits kekinian yang super higienis? Bebas bahan pengawet dan praktis? Kami solusinya!\"', 8, 5, 15000, 'https://cdn.discordapp.com/attachments/776404266767745034/1029191792467390485/opor_ayam.png'),
-(7, 'Kol', 'Pak Fakhri', '\"Kamu butuh sayur dengan harga murah untuk acara kamu? Fresh Mart solusinya!\"', 6, 5, 3000, 'https://cdn.discordapp.com/attachments/776404266767745034/1029191794023477298/kol.png'),
-(8, 'Terong', 'Pak Fadil', '\"Kamu butuh sayur dengan harga murah untuk acara kamu? Fresh Mart solusinya!\"', 13, 3, 5000, 'https://cdn.discordapp.com/attachments/776404266767745034/1029746917602754693/terong.jpg'),
-(9, 'Sayur Asem', 'Pak Thoriq', '\"Setelah membeli produk kami, jangan lupa untuk memberikan testimoni positif.\"', 10, 4, 6000, 'https://cdn.discordapp.com/attachments/776404266767745034/1029191793406918716/sayur_asem.jpg'),
-(10, 'Jagung Manusia', 'Bu Siti', 'Kamu tahu apa yang menjadi kebanggaan kami? Yaitu kepuasan Anda!\"', 10, 4, 5000, 'https://cdn.discordapp.com/attachments/776404266767745034/1029191792865853530/jagung_manis.jpg'),
-(11, 'Sawi', 'Bu Melati', 'Kamu tahu apa yang menjadi kebanggaan kami? Yaitu kepuasan Anda!\"', 8, 3, 2000, 'https://cdn.discordapp.com/attachments/776404266767745034/1029191793679540344/sawi.jpg\r\n');
+INSERT INTO `table_product` (`id`, `title`, `seller`, `description`, `available`, `star`, `price`, `imgLink`, `id_user`) VALUES
+(12, 'timun', 'Rohman', 'Desc', 2, 4, 50000, 'https://cdn.discordapp.com/attachments/776404266767745034/1029191794837172255/timun.png', 1);
 
 --
 -- Indexes for dumped tables
@@ -81,10 +94,17 @@ ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `table_account`
+--
+ALTER TABLE `table_account`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `table_product`
 --
 ALTER TABLE `table_product`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_user` (`id_user`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -97,10 +117,26 @@ ALTER TABLE `migrations`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `table_account`
+--
+ALTER TABLE `table_account`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `table_product`
 --
 ALTER TABLE `table_product`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `table_product`
+--
+ALTER TABLE `table_product`
+  ADD CONSTRAINT `table_product_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `table_account` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
